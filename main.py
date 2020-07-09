da87a3e22b2dc36b3b7d93baf2e75f5b99f43954
martino27 <23117113@njau.edu.cn>
2020-07-09 17:21:18 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "This is a simple server test\n"
+
+
+@app.route("/version")
+def version():
+    return "version1.0\n"
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 9898)
